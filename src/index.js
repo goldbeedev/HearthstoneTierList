5e93227eef1fdcbe677905907aa3cd430a78544e
Justin Goldby <goldbee500@gmail.com>
2018-04-24 23:17:09 -0700

    need to do a few small css fixes, create a write up in the area next to tier buttons, write content about each deck list, link up the weak/strong against and it is ready for soft launch

--- src/index.js
+++ src/index.js
@@ -35,7 +35,10 @@ app.get('/my-data-endpoint', (req, res) => {
 https://api.hearthstonejson.com/v1/23966/enUS/cards.collectible.json
 
 app.get('/decks', function(req, res) {
-    var dstrings = ["AAECAf0GBJ3iAtvpApfTAuCsAg340ALy0ALexALnywKKAbYHiNICi+EC9wSTBPzlAujnAuEHAA==", "AAECAf0EBpvTAqPrAsUE0ALCwwLKwQIM7AeWxwLJA9fhAs7yAk238QLKwwLV 4QKKAevCAqsEAA=="];
+    var dstrings = ["AAECAf0GBMnCApfTAtvpApz4Ag2KAfcEtgfECJvCAufLAvLQAvjQAojSAovhAvzlAujnArfxAgA=", "AAECAZ8FBrnBApvLAsLOArfnAvbsAs30AgzcA/IF9AWWBs8GigevB9kHsQiWCdnHAvjSAgA=", "AAECAZ8FBsUD8gWvB7nBAoPHAtHhAgzbA6cFpwixCLPBAp3CArHCArjHAuPLAvjSAtblAt6CAwA=", "AAECAZICBq8EysMCmdMCguICnOIC0OcCDN4F8gWXwQLrwgKbywLKywKHzgKR0ALR4QL55gLX6wKL7gIA",
+                    "AAECAZ8FBEaiAqcFnvgCDfEF9QX5CpvCAuvCAoPHArjHAuPLApXOAvvTAtHhAtblArXmAgA=", "AAECAf0EBtAC08UCoM4Cws4Cm9MCo+sCDE2KAckD7AebwgKWxwLV4QLX4QKW5AK+7AKm8AK38QIA", "AAECAaIHBLICyAOvBJ74Ag2MAssDmwXUBfUF3QiBwgKfwgLrwgLKwwLR4QKL5QKm7wIA", "AAECAf0EBO0FvwjrwgKi0wINcbsClQOrBLQE5gSWBewFwcECmMQCj9MC++wC7/ECAA==",
+                    "AAECAaIHBM0DhsICz+ECw+oCDcQBnALtAp8DiAWbBdQFhgmXwQL8wQLH0wLb4wL27AIA", "AAECAZICBpAHysMCh84Cws4Cr9MCxfMCDEBf6QHkCMnHApTSApjSAp7SAovhAoTmAo3wAr/yAgA=", "AAECAa0GBsUEigeQB9YKkNMCifECDKEE5QT2B40I0wryDNHBAsrDAsnHAujQAsvmAr3zAgA=", "AAECAaoICPUE9QX/BfPCApziAqfuAp7wAu/3AguBBP4FkwmXwQKZwgLrwgLKwwKHxAKbywL70wLf6QIA",
+                    "AAECAR8CrwSe+AIOoQKiAqgCtQP1Be0GlwjbCf4Ml8EC68IClc4C4eMCi+UCAA==", "AAECAQcG0gLyBcLOAp/TAs3vApvwAgwckAOOBZEGigfrwgLMzQK67AKF7QKd8AKX8wLR9QIA", "AAECAaIHBLIC7QKvBIHCAg20AYwCzQO9BJsFiAekB90IhgnrwgLc0QLb4wKm7wIA", "AAECAaIHBLICrwTCzgKc4gINtAGMAu0C8gXdCIHCAuvCAsrDApvLAsrLAtvjAqbvAsf4AgA="];
     var decoded = [];
     for (var i = 0; i < dstrings.length; i++){
       decoded.push(decode(dstrings[i]));
